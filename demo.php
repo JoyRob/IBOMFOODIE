e85ea1c9c613cb058b37b866a00b26600bc8445c
JoyRob <you@example.com>
2024-02-16 01:16:43 +0100

    first commit

---
+++ demo.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+	</head>
+	<body>
+        <!-- ========== HEADER ========== -->
+<?php 
+include('components/header.php')
+?>
+
+<!-- Hero -->
+<!-- Features -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <!-- Grid -->
+  <div class="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
+    <div>
+      <img class="rounded-xl" src="https://images.unsplash.com/photo-1648737963503-1a26da876aca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&h=900&q=80" alt="Image Description">
+    </div>
+    <!-- End Col -->
+
+    <div class="mt-5 sm:mt-10 lg:mt-0">
+      <div class="space-y-6 sm:space-y-8">
+        <!-- Title -->
+        <div class="space-y-2 md:space-y-4">
+          <h2 class="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
+            We tackle the challenges start-ups face
+          </h2>
+          <p class="text-gray-500">
+            Besides working with start-up enterprises as a partner for digitalization, we have built enterprise products for common pain points that we have encountered in various products and projects.
+          </p>
+        </div>
+        <!-- End Title -->
+
+        <!-- List -->
+        <ul role="list" class="space-y-2 sm:space-y-4">
+          <li class="flex space-x-3">
+            <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+            </span>
+
+            <span class="text-sm sm:text-base text-gray-500">
+              <span class="font-bold">Easy & fast</span> designing
+            </span>
+          </li>
+
+          <li class="flex space-x-3">
+            <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+            </span>
+
+            <span class="text-sm sm:text-base text-gray-500">
+              Powerful <span class="font-bold">features</span>
+            </span>
+          </li>
+
+          <li class="flex space-x-3">
+            <span class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg class="flex-shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+            </span>
+
+            <span class="text-sm sm:text-base text-gray-500">
+              User Experience Design
+            </span>
+          </li>
+        </ul>
+        <!-- End List -->
+      </div>
+    </div>
+    <!-- End Col -->
+  </div>
+  <!-- End Grid -->
+</div>
+<!-- End Features -->
+<!-- Features -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 bg-v lg:py-14 bg-violet-100 mx-auto">
+  <!-- Grid -->
+  <div class="mt-10 grid gap-6 grid-cols-2 sm:gap-12 lg:grid-cols-3 lg:gap-8">
+    <!-- Stats -->
+    <div>
+      <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Accuracy rate</h4>
+      <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">99.95%</p>
+      <p class="mt-1 text-gray-500">in fulfilling orders</p>
+    </div>
+    <!-- End Stats -->
+
+    <!-- Stats -->
+    <div>
+      <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Chefs and menus</h4>
+      <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">2,000+</p>
+      <p class="mt-1 text-gray-500">partner with Preline</p>
+    </div>
+    <!-- End Stats -->
+
+    <!-- Stats -->
+    <div>
+      <h4 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-gray-200">Happy customer</h4>
+      <p class="mt-2 sm:mt-3 text-4xl sm:text-6xl font-bold text-blue-600">85%</p>
+      <p class="mt-1 text-gray-500">this year alone</p>
+    </div>
+    <!-- End Stats -->
+  </div>
+  <!-- End Grid -->
+</div>
+<!-- End Features -->
+<!-- End Hero -->
+<!-- ========== END HEADER ========== -->
+   <!-- Card Blog -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <!-- Grid -->
+  <div class="grid lg:grid-cols-2 lg:gap-y-16 gap-10">
+    <!-- Card -->
+    <a class="group rounded-xl overflow-hidden dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+      <div class="sm:flex">
+        <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+          <img class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl" src="https://images.unsplash.com/photo-1540575861501-7cf05a4b125a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description">
+        </div>
+
+        <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+          <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
+            Studio by Preline
+          </h3>
+          <p class="mt-3 text-gray-600 dark:text-gray-400">
+            Produce professional, reliable streams easily leveraging Preline's innovative broadcast studio
+          </p>
+          <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+            Read more
+            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+          </p>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="group rounded-xl overflow-hidden dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+      <div class="sm:flex">
+        <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+          <img class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl" src="https://images.unsplash.com/photo-1668906093328-99601a1aa584?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80" alt="Image Description">
+        </div>
+
+        <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+          <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
+            Onsite
+          </h3>
+          <p class="mt-3 text-gray-600 dark:text-gray-400">
+            Optimize your in-person experience with best-in-class capabilities like badge printing and lead retrieval
+          </p>
+          <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+            Read more
+            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+          </p>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="group rounded-xl overflow-hidden dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+      <div class="sm:flex">
+        <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+          <img class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl" src="https://images.unsplash.com/photo-1567016526105-22da7c13161a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" alt="Image Description">
+        </div>
+
+        <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+          <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
+            The complete guide to OKRs
+          </h3>
+          <p class="mt-3 text-gray-600 dark:text-gray-400">
+            How to make objectives and key results work for your company
+          </p>
+          <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+            Read more
+            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+          </p>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="group rounded-xl overflow-hidden dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+      <div class="sm:flex">
+        <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
+          <img class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl" src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description">
+        </div>
+
+        <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0">
+          <h3 class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
+            People program models
+          </h3>
+          <p class="mt-3 text-gray-600 dark:text-gray-400">
+            Six approaches to bringing your People strategy to life
+          </p>
+          <p class="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium">
+            Read more
+            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+          </p>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+  </div>
+  <!-- End Grid -->
+</div>
+<!-- End Card Blog -->
+<?php include('components/footer.php') ?>
+
+	</body>
+</html>
