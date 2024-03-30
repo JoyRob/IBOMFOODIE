28b6197de7f0b1a3520e264f4160430208bdb43e
JoyRob <you@example.com>
2024-03-30 19:31:52 +0100

    login set up

--- login.php
+++ login.php
@@ -1,100 +1,38 @@
-<?php
-session_start();
-
-include("db.php");
-include("logincheck.php");
-
-
-if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-
-		if(!empty($email) && !empty($password) && !is_numeric($email))
-		{
-
-			//read from database
-			$query = "select * from users where email = '$email' limit 1";
-			$result = mysqli_query($con, $query);
-
-			if($result)
-			{
-				if($result && mysqli_num_rows($result) > 0)
-				{
-
-					$user_data = mysqli_fetch_assoc($result);
-					
-					if($user_data['password'] === $password)
-					{
-
-						$_SESSION['user_id'] = $user_data['user_id'];
-						header("Location: index.php");
-						die;
-					}
-				}
-			}
-			
-			echo "wrong username or password!";
-		}else
-		{
-			echo "wrong username or password!";
-		}
-	}
-
-?>
-
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN PAGE</title>
+<link rel="stylesheet" href="css/login.css">
 </head>
 <body>
+<form action="action_page.php" method="post">
+  <div class="imgcontainer">
+    <img src="images/83A23CAF-8340-4ED8-81D9-4270837A3DDD.png" alt="Avatar" class="avatar" id="login">
+  </div>
 
-	<style type="text/css">
-	
-	#text{
+  <div class="container">
+    <label for="uname"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required><br>
 
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	
-	}
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required><br>
 
-	#button{
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
 
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-
-	</style>
-
-	<div id="box">
-		
-		<form method="post">
-			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
-
-			<input id="text" type="text" name="email"><br><br>
-			<input id="text" type="password" name="password"><br><br>
-
-			<input id="button" type="submit" value="Login"><br><br>
-
-			<a href="signup.php">Click to Signup</a><br><br>
-		</form>
-	</div>
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
 </body>
+<footer>
+
+</footer>
 </html>
+
