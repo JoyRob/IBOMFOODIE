f330a4d15ea2d49baf182c351cc5bf2f06caf1d2
JoyRob <you@example.com>
2024-04-08 17:38:20 +0200

    update

--- components/header.php
+++ components/header.php
@@ -1,27 +1,65 @@
-<header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-violet-800 text-sm py-3 sm:py-0">
-  <nav class="relative max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
-    <div class="flex items-center justify-between">
-      <a class="flex-none text-xl font-semibold text-white" href="#" aria-label="Brand">Brand</a>
-      <div class="sm:hidden">
-        <button type="button" class="hs-collapse-toggle size-9 flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-white/20 text-white hover:border-white/40 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
-          <svg class="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
-          <svg class="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
-        </button>
-      </div>
-    </div>
-    <div id="navbar-collapse-with-animation" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
-      <div class="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7">
-        <a class="font-medium text-white sm:py-6" href="#" aria-current="page">Home</a>
-        <a class="font-medium text-white/[.8] hover:text-white sm:py-6" href="#">About us</a>
-        <a class="font-medium text-white/[.8] hover:text-white sm:py-6" href="#">Contact us</a>
-        <a class="font-medium text-white/[.8] hover:text-white sm:py-6" href="#">Menu</a>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MY AKWAIBOM KITCHEN</title>
+    <link rel="stylesheet" href="css/thefooter.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/unsemantic-grid-responsive-tablet.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-        <a class="flex items-center gap-x-2 font-medium text-white/[.8] hover:text-white sm:border-s sm:border-white/[.3] sm:my-6 sm:ps-6" href="#">
-          <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
-          Log in
-        </a>
-      </div>
-    </div>
-  </nav>
-</header>
+    <body class="body">
+    <HEAder>
+        <header>
+            <nav class="navbar border py-0
+             navbar-expand-sm navbar-dark bg-BLACK">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="javascript:void(0)"><img
+                            src="images/83A23CAF-8340-4ED8-81D9-4270837A3DDD.png" alt="logo" id="buisnesslogo"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#mynavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="mynavbar">
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">HOME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="Aboutus.php">ABOUT US</a>
+                            </li>
+                            <?php
+                            session_start();
+                            if (isset($_SESSION["user_id"])) {
+                                // If user is logged in, show different links
+                                echo '
+                                <li class="nav-item">
+                                    <a class="nav-link" href="Recipelist.php">VIEW RECIPE</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="logout.php">LOGOUT</a>
+                                </li>
+                                ';
+                            } else {
+                                // If user is not logged in, show sign up and login links
+                                echo '
+                                <li class="nav-item">
+                                    <a class="nav-link" href="signup.php">SIGN UP</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="login.php">LOGIN</a>
+                                </li>
+                                ';
+                            }
+                            ?>
+                        </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="text" placeholder="Search">
+                            <button class="btn btn-primary" type="button">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
